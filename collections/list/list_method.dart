caf4b486e215d5992c0b1e7b1d53b8bcc87c74cd
Roman-Dewan <mdromandewan1888@gmail.com>
2026-07-27 16:49:09 +0600

    list

--- collections/list/list_method.dart
+++ collections/list/list_method.dart
@@ -1,12 +1,42 @@
-/// fold
-
 void main(List<String> args) {
-  List<double> result = [10, 2];
-  const value1 = 50.0;
+  /// fold
+  /*
+  List<double> result = [3, 2];
+  const initialValue = 2.0;
+
+  /// it always do calculation with the initialValue,
+  /// the initialValue = 3
+  /// the initialValue start summing with the list one by one.
+  /// 1. initialValue = 2.0
+  /// 2. then it update with like initialValue += result[0] --- that is 2.0 + 3 = 5.0
+  ///   now initialValue = 5.0
+  /// 3. again initialvalue += result[1] --- that is 5.0 + 2 = 7.0
+  ///
+  /// then the reuslt1 = initialValue = 7.0
 
   final double result1 = result.fold<double>(
-    value1,
-    (start, end) => start / end,
+    initialValue,
+    (start, end) => (start + end),
   );
   print(result1);
+  */
+
+  List<int> data = [1, 2, 3];
+  List<int> data1 = [11, 22, 33];
+
+  /// add
+  data.add(5);
+  print(data);
+
+  /// add all
+  data.addAll(data1);
+  print(data);
+
+  /// clear
+  data1.clear();
+  print(data1);
+
+  /// any
+  bool result = data.any((value) => value >= 5);
+  print(result);
 }
--- collections/list/list_properties.dart
+++ collections/list/list_properties.dart
@@ -1,19 +1,28 @@
 import 'dart:core';
+import 'dart:js_interop';
 
 void main(List<String> args) {
   print("\n---------properties------\n");
   List num = <int>[0, 1, 2, 3, 4];
+  List num1 = <int>[5];
 
-  print(num.length);
-  print(num.reversed.toList());
-  print(num.first);
-  print(num.isEmpty);
-  print(num.isNotEmpty);
-  print(num.iterator);
-  print(num.last);
-  print(num.runtimeType);
-  print(num.firstOrNull);
+  print("num.length: ${num.length}");
+  print("num.reversed.toList(): ${num.reversed.toList()}");
 
-  // print(num.single);
-  
+  print("num.first: ${num.first}");
+  print("num.last: ${num.last}");
+
+  print("num.isEmpty: ${num.isEmpty}");
+  print("num.isNotEmpty: ${num.isNotEmpty}");
+
+  print("num.iterator: ${num.iterator}");
+  print("num.runtimeType: ${num.runtimeType}");
+
+  print("num.firstOrNull: ${num.firstOrNull}");
+  print("num.lastOrNull: ${num.lastOrNull}");
+
+  print("num.hashCode: ${num.hashCode}");
+
+  print("num.nonNulls: ${num.nonNulls.toList()}");
+  print("num1.single: ${num1.single}");
 }
